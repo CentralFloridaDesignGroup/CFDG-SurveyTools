847b66d640d046e3db11809b949a96b606020985
Nathan White <nwhitemisc@gmail.com>
2022-08-01 15:06:07 -0400

    Start of configuration manager.

---
+++ Applications/ConfigurationManager/AutoCADVersionModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace ConfigurationManager
+{
+    public class AutoCADVersionModel
+    {
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private string _configFile;
+        public string ConfigFile
+        {
+            get { return _configFile; }
+            set { _configFile = value; }
+        }
+
+        private BitmapImage _image;
+        public BitmapImage Image
+        {
+            get { return _image; }
+            set { _image = value; }
+        }
+    }
+}
